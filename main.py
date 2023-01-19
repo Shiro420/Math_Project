9a646202c2732752e8fe87b91380c2ba0aa8d5f6
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-01-19 12:17:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import numpy as np
 from tkinter import *
 
 root = Tk()
+root.title("log in")
+root.geometry("400x400")
+root.resizable(width=0, height=0)
 
-def lineare_funktion():
-    von_label = Label(root, text="anfang: ")
-    von_entry = Entry(root)
-    bis_label = Label(root, text="ende: ")
-    bis_entry = Entry(root)
-    m_Label = Label(root, text="m").grid(row=0)
-    m_entry = Entry(root)
-    b_label = Label(root,text="b").grid(row=1)
-    b_entry = Entry(root)
-    x_label = Label(root, text="x").grid(row=2)
-    x_entry = Entry(root)
-    y_label = Label(root, text="y").grid(row=3)
-    y_entry = Entry(root)
-    x_achse_label = Label(root, text="x_Achse")
-    x_achse_entry = Entry(root)
-    y_achse_label = Label(root, text="y_achse")
-    y_achse_entry = Entry(root)
-    
-def Menu() :
-    menu = tk.menu(root,tearoff = 0)
-    root.config(menu = menu)
-    menu.add_cascade(label = "lineare",command = lineare_funktion)
-    menu.add_cascade(label = "Quadratische")
-    menu.add_cascade(label = "Ganzrationale")
-    menu.add_command(label="Trigonometrische")
-    menu.add_command(label="Exponential")
-    menu.add_command(label="Differenzial")
-    menu.add_command(label="Kurvendiskusion")
-    menu.add_command(label="Integralrechnung")
+def login ():
+    lnfenster = Tk()
+    lnfenster.title("log in Fenster")
+    lnfenster.geometry("600x600")
+    lnfenster.resizable(width=0,height=0)
+    Funktionen = StringVar(lnfenster)
+    Funktionen.set("Funktionen")
 
-root.mainloop
+    def show(selection):
+        Auswahl = selection
+        fenster = Tk()
+        fenster.geometry("600x600")
+        fenster.resizable(width=0,height=0)
+        fenster.title(Auswahl)
 
+        if Auswahl == "Linearefunktion":
+            butoon1 = Button(fenster, text="Ausführen", )
+            ytext = Entry(fenster, bd=5, width=7)
+            xtext = Entry(fenster, bd=5, width=7)
+            btext = Entry(fenster, bd=5, width=7)
+            mtext = Entry(fenster, bd=5, width=7)
 
+    drop = OptionMenu(root,"Lineare-Funktion" , "Quadratische-Funktion" ,
+                      "Ganzrationale-Funktionen" ,"Trigonometrische-Funktionen" , "Exponential-Funktionen" ,
+                      "Einstieg-Differenzialrechnung" , "Kurvendiskussion", "Integralrechnung",)
+    drop.pack()
+    drop.place(relx=0.0, rely=0.0)
 
-
-
-
-
-
+root.mainloop()
