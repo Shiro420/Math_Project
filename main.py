1964927754b0b6e8538b881965ccc3832bd6e071
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-01-21 16:14:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 from tkinter import *
 from tkinter import ttk
+
 root = Tk()
-root.title("main window")
+root.title("main_window")
 root.geometry("400x400")
 root.resizable(width=0, height=0)
+def search(event):
 
-
-def show(select):
     fenster = Tk()
     fenster.title("main window")
     fenster.geometry("400x400")
     fenster.resizable(width=0, height=0)
 
     if cmb.get() == "linear":
-        
-	elif cmb.get() == "Qudratisch":
-    
+
+
+    elif cmb.get() == "Qudratisch":
+
+
     elif cmb.get() == "Ganzrationale":
-    
+
+
     elif cmb.get() == "Trigonometrische":
-    
-    elif cmb.get()	== "Exponentail":
-        
+
+
+    elif cmb.get() == "Exponentail":
+
+
     elif cmb.get() == "Einstig-Defferenzilrechnung":
-        
+
+
     elif cmb.get() == "Kurvendiskussion":
-        
-    elif mcb.get() == "Integralrechnung": 
-    
-        return
 
+
+    elif cmb.get() == "Integralrechnung":
+
+    return
+
+
+button1 = Button(root, text="Ausführen",command=search(event=0),)
 funktionen = ("Linear", "Qudratisch", "Ganzrationale", "Trigonometrische", "Exponential",
-                      "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung")
-
+              "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung")
 l1 = Label(root, text="Funktionenen auswahl")
 l1.grid(row=0, column=0)
+button1.grid(row=1,column=1)
 cmb = ttk.Combobox(root, value=funktionen, width=15)
 cmb.set('Funktionen')
-
 cmb.grid(row=1, column=0)
-
-
 root.mainloop()
