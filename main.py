85db778d42830bcaa1d8495d11f06e85c0a94bae
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-01-22 17:12:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ root = Tk()
 root.title("main_window")
 root.geometry("400x400")
 root.resizable(width=0, height=0)
-def search(event):
-
+def search(selection):
     fenster = Tk()
     fenster.title("main window")
     fenster.geometry("400x400")
     fenster.resizable(width=0, height=0)
 
+
     if cmb.get() == "linear":
-
-
+        
     elif cmb.get() == "Qudratisch":
 
 
@@ -38,13 +37,14 @@ def search(event):
     return
 
 
-button1 = Button(root, text="Ausführen",command=search(event=0),)
 funktionen = ("Linear", "Qudratisch", "Ganzrationale", "Trigonometrische", "Exponential",
               "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung")
 l1 = Label(root, text="Funktionenen auswahl")
 l1.grid(row=0, column=0)
-button1.grid(row=1,column=1)
+
 cmb = ttk.Combobox(root, value=funktionen, width=15)
 cmb.set('Funktionen')
+button1 = Button(root, text="Ausführen",command=search(selection=funktionen))
+button1.grid(row=1,column=1)
 cmb.grid(row=1, column=0)
 root.mainloop()
