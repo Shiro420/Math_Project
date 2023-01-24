0befd58b89adfb4bbb9cf9243e40b901dcb7225b
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-01-24 09:10:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,56 @@ root.title("main_window")
 root.geometry("400x400")
 root.resizable(width=0, height=0)
 
+
 def search(selection):
     fenster = Tk()
-    fenster.title("main window")
+    fenster.title("naja")
     fenster.geometry("400x400")
     fenster.resizable(width=0, height=0)
+    Auswahl = cmb.get()
+    if Auswahl == "linear":
+        butoon1 = Button(fenster, text="Ausführen", )
+        butoon2 = Button(fenster, text="Beispiele", )
+        butoon3 = Button(fenster, text="Zoom-in", )
+        butoon4 = Button(fenster, text="Zoom-out", )
+        ytext = Entry(fenster, bd=5, width=7)
+        xtext = Entry(fenster, bd=5, width=7)
+        btext = Entry(fenster, bd=5, width=7)
+        mtext = Entry(fenster, bd=5, width=7)
+        xbeschriftung = Entry(fenster, bd=5, width=7)
+        ybeschriftung = Entry(fenster, bd=5, width=7)
+
+        ytext.pack()
+        xtext.pack()
+        btext.pack()
+        mtext.pack()
+        xbeschriftung.pack()
+        ybeschriftung.pack()
+        butoon4.pack()
+        butoon3.pack()
+        butoon2.pack()
+        butoon1.pack()
+
+        ytext.place(relx=2.1, rely=7.8), xtext.place(relx=4.1, rely=6.8)
+    elif Auswahl == "Qudratisch":
 
 
-    if cmb.get() == "linear":
-        
-    elif cmb.get() == "Qudratisch":
+    elif Auswahl == "Ganzrationale":
 
 
-    elif cmb.get() == "Ganzrationale":
+    elif Auswahl == "Trigonometrische":
 
 
-    elif cmb.get() == "Trigonometrische":
+    elif Auswahl == "Exponentail":
 
 
-    elif cmb.get() == "Exponentail":
+    elif Auswahl == "Einstig-Defferenzilrechnung":
 
 
-    elif cmb.get() == "Einstig-Defferenzilrechnung":
+    elif Auswahl == "Kurvendiskussion":
 
 
-    elif cmb.get() == "Kurvendiskussion":
-
-
-    elif cmb.get() == "Integralrechnung":
+    elif Auswahl == "Integralrechnung":
 
     return
 
@@ -41,12 +63,13 @@ def search(selection):
 funktionen = ("Linear", "Qudratisch", "Ganzrationale", "Trigonometrische", "Exponential",
               "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung")
 
+
 l1 = Label(root, text="Funktionenen auswahl")
 cmb = ttk.Combobox(root, value=funktionen, width=15)
 cmb.set('Funktionen')
-button1 = Button(root, text="Ausführen",command=search(selection=funktionen))
+cmb.bind("<<ComboboxSelected>>",search)
 
 l1.grid(row=0, column=0)
-button1.grid(row=1,column=1)
 cmb.grid(row=1, column=0)
 root.mainloop()
+
