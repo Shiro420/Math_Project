001f2b9598f549934f04ebed50ecc604577132ad
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-03-24 15:53:41 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,20 @@
 import sqlite3
 
+
 def create_db():
     con = sqlite3.connect("mathe.db")
     cur = con.cursor()
 
-   
     sql = "CREATE TABLE user (" \
           "username TEXT, " \
           "passwort TEXT);"
     cur.execute(sql)
     con.commit()
-    
+
     Benutzer = "admin"
     Passwort = "admin"
     insert = f"INSERT INTO anmeldung VALUES('{Benutzer}', '{Passwort}');"
-    cursor.execute(insert)
+    cur.execute(insert)
     con.close()
-creat_db()
+
+create_db()
