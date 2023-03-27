c446ce9d4177b2728e1d4b5c074c911e8fcd8021
Zorua <120640267+Shiro420@users.noreply.github.com>
2023-03-27 16:27:36 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 
 def create_db():
     con = sqlite3.connect("mathe.db")
@@ -17,4 +17,52 @@ def create_db():
     cur.execute(insert)
     con.close()
 
-create_db()
+def NewAcc(name, passwort):
+    connection = sqlite3.connect("mathe.db")
+    cursor = connection.cursor()
+    InsertNewAcc = f"INSERT INTO anmeldung VALUES('{name}', '{passwort}');"
+    cursor.execute(InsertNewAcc)
+    connection.commit()
+    connection.close()
+
+def Prüfanm(name, passwort):
+    con = sqlite3.connect("mathe.db")
+    cur= con.cursor()
+
+    statement = f"""SELECT benutzername
+                    FROM anmeldung
+                    WHERE benutzername ='{name}' AND passwort= '{passwort}'"""
+    cur.execute(statement)
+    k = cur.fetchone()
+    if not k:
+        return False
+    else:
+        return True
+def regristierungs_Prüfen(Benutzer):
+    con = sqlite3.connect("mathe.db")
+    cur = con.cursor()
+
+    sql = f"""SELECT benutzername 
+          FROM anmeldung 
+          WHERE benutzername = '{Benutzer}';"""
+
+    PrüfRgst = cur.execute(sql).fetchone()
+    if PrüfRgst:
+        return False
+    else:
+        return True
+
+def Ausgabe():
+    con = sqlite3.connect("mathe.db")
+    cur = con.cursor()
+
+    sql = "SELECT * FROM anmeldung"
+    ausgabe = cur.execute(sql).fetchall()
+    print(ausgabe)
+    con.commit()
+    con.close()
+
+if os.path.exists("mathe.db"):
+    print("Datenbank existiert bereits")
+else:
+    create_db()
